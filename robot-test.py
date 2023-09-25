f15ae768207573826fd3b38101eabc0a166865d9
Zach Buchar <kkzabuchar@gmail.com>
2023-09-24 20:17:09 -0500

    Add robot connection test script

---
+++ robot-test.py
@@ -0,0 +1,17 @@
+from weallcode_robot import Robot
+
+spacer = "+--------------------+"
+
+print("Python Robot Tester\n{}\n".format(spacer))
+robotName = input("Enter robot name: ")
+robotTester = Robot(robotName)
+
+print("\nVerify that LEDs turn on\n\n{}\n".format(spacer))
+
+robotTester.led(255, 0, 0, 2)
+
+try:
+    robotTester.run()
+except Exception as error:
+    print("❌ Error: {}".format(error))
+
